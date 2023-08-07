270ed70b1ff534dc54bd9abef5838613acd0362e
Patrick <patrickz.t@outlook.com>
2023-08-07 11:55:48 -0300

    Desafio intelitrader

---
+++ desafio1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace IntersectFilesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o caminho do primeiro arquivo: ");
+            string filePath1 = Console.ReadLine();
+            Console.Write("Digite o caminho do segundo arquivo: ");
+            string filePath2 = Console.ReadLine();
+            Console.Write("Digite o nome do arquivo de saída (com extensão): ");
+            string outputFileName = Console.ReadLine();
+
+            try
+            {
+                string[] lines1 = File.ReadAllLines(filePath1);
+                string[] lines2 = File.ReadAllLines(filePath2);
+
+                using (StreamWriter writer = new StreamWriter(outputFileName))
+                {
+                    foreach (string line1 in lines1)
+                    {
+                        foreach (string line2 in lines2)
+                        {
+                            if (line1 == line2)
+                            {
+                                writer.WriteLine(line1);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine("Interseção das linhas gravada com sucesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ocorreu um erro: {e.Message}");
+            }
+        }
+    }
+}
---
+++ desafio2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace desafioCriptografia
+{
+  class CifraDeCesar
+  {
+    static string Criptografar(string mensagem, int deslocamento)
+    {
+      var criptografada = "";
+
+      foreach (char letra in mensagem)
+      {
+        if (char.IsLetter(letra))
+        {
+          char limiteAlfabeto = char.IsUpper(letra) ? 'Z' : 'z';
+          char deslocada = (char)(letra + deslocamento);
+
+          if (deslocada > limiteAlfabeto)
+          {
+            deslocada = (char)(deslocada - 26);
+          }
+          criptografada += deslocada;
+        }
+        else
+        {
+          criptografada += letra;
+        }
+      }
+
+      return criptografada;
+    }
+
+    static string Descriptografar(string mensagem, int deslocamento)
+    {
+      return Criptografar(mensagem, -deslocamento);
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Escolha uma opção:");
+      Console.WriteLine("1 - Criptografar");
+      Console.WriteLine("2 - Descriptografar");
+
+      char escolha = Console.ReadKey().KeyChar;
+      Console.WriteLine();
+
+      if (escolha != '1' && escolha != '2')
+      {
+        Console.WriteLine("Erro: Escolha inválida.");
+        return;
+      }
+
+      Console.Write("Digite o caminho do arquivo de entrada: ");
+      string arquivoEntrada = Console.ReadLine();
+
+      if (!File.Exists(arquivoEntrada))
+      {
+        Console.WriteLine("Erro: O arquivo de entrada não existe.");
+        return;
+      }
+
+      Console.Write("Digite o caminho do arquivo de saída: ");
+      string arquivoSaida = Console.ReadLine();
+
+      Console.Write("Digite um valor de deslocamento: ");
+      int deslocamento = int.Parse(Console.ReadLine());
+
+      string conteudo = File.ReadAllText(arquivoEntrada);
+      string resultado = "";
+
+      switch (escolha)
+      {
+        case '1':
+          resultado = Criptografar(conteudo, deslocamento);
+          break;
+
+        case '2':
+          resultado = Descriptografar(conteudo, deslocamento);
+          break;
+      }
+
+      File.WriteAllText(arquivoSaida, resultado);
+      Console.WriteLine("Resultado gravado no arquivo de saída.");
+    }
+  }
+}
+
